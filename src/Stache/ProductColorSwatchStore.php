3d40387bb281d5cf12aa51e0c3906d975e016240
Jacob Butterworth <jacob.b@weareframework.co.uk>
2024-09-30 13:28:55 +0100

    feat: will this work ??

--- src/Stache/ProductColorSwatchStore.php
+++ src/Stache/ProductColorSwatchStore.php
@@ -18,22 +18,32 @@ class ProductColorSwatchStore extends BasicStore
         return 'product-color-swatches';
     }
 
+    public function arrayPull(Array $array, $key, $default = null)
+    {
+        if (function_exists('array_pull')) {
+            return array_pull($array, $key, $default);
+        }
+
+        return \Statamic\Support\Arr::pull($array, $key, $default);
+    }
+
     public function makeItemFromFile($path, $contents)
     {
         $data = YAML::file($path)->parse($contents);
 
-        if (! $id = array_pull($data, 'id')) {
+        
+        if (! $id = $this->arrayPull($data, 'id')) {
             $idGenerated = true;
             $id = app('stache')->generateId();
         }
 
         $productColorSwatch = ProductColorSwatch::make()
             ->id($id)
-            ->src(array_pull($data, 'src'))
-            ->key(array_pull($data, 'key'))
-            ->name(array_pull($data, 'name'))
-            ->enabled(array_pull($data, 'enabled'))
-            ->colors(array_pull($data, 'colors'))
+            ->src($this->arrayPull($data, 'src'))
+            ->key($this->arrayPull($data, 'key'))
+            ->name($this->arrayPull($data, 'name'))
+            ->enabled($this->arrayPull($data, 'enabled'))            
+            ->colors($this->arrayPull($data, 'colors'))
             ->initialPath($path);
 
         if (isset($idGenerated)) {
